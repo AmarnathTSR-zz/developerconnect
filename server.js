26802c2a91a0e416fd7e87176aed43d051929fa7
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-13 18:46:50 +0530

    Port 3000 change to 5000 for express

--- server.js
+++ server.js
@@ -46,10 +46,10 @@ app.use('/api/posts', posts);
 
 
 
-//  Assigning the port eg. http://localhost/3000 for local environment for heroku we use process.env.port
-const port = process.env.PORT || 3000;
+//  Assigning the port eg. http://localhost:5000 for local environment for heroku we use process.env.port
+const port = process.env.PORT || 5000;
 
-// finally app listen the port 3000
+// finally app listen the port 5000
 app.listen(port, () => {
   console.log(`Port listen on : ${port}`);
 });
