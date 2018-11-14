cfe75d621516d0d0dd8f9e9832fb3482b7932688
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-14 13:35:26 +0530

    alignment

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
 //  Import the external libraries
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const passport = require("passport");
 
 // Include custom Router we created
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 // Import Keys
-const uri = require('./config/keys').mongoURI;
+const uri = require("./config/keys").mongoURI;
 
 // Initialize the app
 const app = express();
 // parse application/x-www-form-urlencoded
-app.use(bodyparser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyparser.urlencoded({
+    extended: false
+  })
+);
 
 // parse application/json
 app.use(bodyparser.json());
 
 mongoose
-  .connect(uri, {
-    useNewUrlParser: true
-  })
+  .connect(
+    uri,
+    {
+      useNewUrlParser: true
+    }
+  )
   .then(() => console.log("DB connected success"))
   .catch(err => console.log(err));
 
@@ -36,15 +41,17 @@ app.use(passport.initialize());
 
 // include passport stratagy
 
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-// Use our router 
+// Use our router
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-
+app.get("/", (req, res) => {
+  res.json({ success: "welcome to amarnath rest api" });
+});
 
 //  Assigning the port eg. http://localhost:5000 for local environment for heroku we use process.env.port
 const port = process.env.PORT || 5000;
