f5dac58865e1aacaf8c2c0eaecbdce8337da46e7
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-15 16:49:43 +0530

    import material design component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+  MatInputModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatButtonModule
+} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
