e1acda4b3a76642cc15aeb72864f7428481a5d32
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-15 17:33:37 +0530

    created home and register component
    i have created register component for user registration and for home pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,8 +11,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent],
 
   imports: [
     BrowserModule,
