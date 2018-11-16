c3ecb9a2eb38d4bcc5c4f6f83e4c0c77843d3134
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-16 13:00:10 +0530

    allow access control origin

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 //  Import the external libraries
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const passport = require('passport');
 
 // Include custom Router we created
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 // Import Keys
-const uri = require("./config/keys").mongoURI;
+const uri = require('./config/keys').mongoURI;
 
 // Initialize the app
 const app = express();
@@ -21,6 +21,11 @@ app.use(
     extended: false
   })
 );
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // parse application/json
 app.use(bodyparser.json());
@@ -32,7 +37,7 @@ mongoose
       useNewUrlParser: true
     }
   )
-  .then(() => console.log("DB connected success"))
+  .then(() => console.log('DB connected success'))
   .catch(err => console.log(err));
 
 // passport middleware added
@@ -41,16 +46,16 @@ app.use(passport.initialize());
 
 // include passport stratagy
 
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Use our router
 
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-app.get("/", (req, res) => {
-  res.json({ success: "welcome to amarnath rest api" });
+app.get('/', (req, res) => {
+  res.json({ success: 'welcome to amarnath rest api' });
 });
 
 //  Assigning the port eg. http://localhost:5000 for local environment for heroku we use process.env.port
