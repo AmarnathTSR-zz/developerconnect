c657443643322c4f511c92e19bf3bb9a0509a49f
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-16 10:55:54 +0530

    added html in landing

--- reactclient/src/components/layout/Landing.js
+++ reactclient/src/components/layout/Landing.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 export default class Landing extends Component {
   render() {
     return (
@@ -8,15 +8,13 @@ export default class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <h1 className="display-3 mb-4">Connect</h1>
                 <p className="lead">
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  Create a developer profile/portfolio, share posts and get help from other
+                  developers
                 </p>
                 <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </Link>
+
                 <Link to="/login" className="btn btn-lg btn-light">
                   Login
                 </Link>
