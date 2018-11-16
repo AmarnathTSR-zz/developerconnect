715c7ccd4b9770bb0dc3f2dd8bd95943ee2af0d3
Amarnath TSR <tsr.amarnath@gmail.com>
2018-11-16 13:16:14 +0530

    import classnames from classnames

--- reactclient/src/components/auth/login.js
+++ reactclient/src/components/auth/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
 export default class login extends Component {
   constructor() {
     super();
